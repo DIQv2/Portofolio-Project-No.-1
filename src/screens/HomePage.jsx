f32f9681194bb03cb27be9c4f79186e0eb6d5546
DragosBasca <dragos.basca@sde.center>
2024-10-19 17:45:57 +0300

    fixed desktop styling ':O

--- src/screens/HomePage.jsx
+++ src/screens/HomePage.jsx
@@ -3,11 +3,11 @@ import GrayBackground from "../assets/design/GrayBackground";
 
 const HomePage = () => {
     return (
-        <div className="w-screen h-[100dvh] snap-y snap-mandatory overflow-y-scroll overflow-x-hidden hide-scrollbar scroll-smooth"
+        <div className="w-screen h-[100dvh] md:h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden hide-scrollbar scroll-smooth"
         >
             <GrayBackground />
             {homepageScreens.map((e, index) => (
-                <div className="relative w-screen h-[100dvh] snap-center select-none z-10" key={index}>
+                <div className="relative w-screen h-[100dvh] md:h-screen snap-center select-none z-10" key={index}>
                     {e}
                 </div>
             ))}
